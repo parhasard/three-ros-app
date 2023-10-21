9b0e0f67cc4835ae041b71cf04a14c30c9cd9f31
parhasard <hazar_karadag@hotmail.com>
2023-10-21 22:36:28 +0200

    add canvas styling

--- src/App.vue
+++ src/App.vue
@@ -7,90 +7,33 @@
   </div>
 </template>
 
-<script lang="ts">
-import { ref, onMounted,  defineComponent } from 'vue'
-import { IThreeHelper } from '@/helpers/threeHelpers/interfaces/IThreeHelper'
-import { ThreeHelper } from '@/helpers/threeHelpers/core/ThreeHelper'
-import { EnhancedThreeHelper } from '@/helpers/threeHelpers/core/EnhancedThreeHelper.ts'
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import type { IThreeHelper } from '@/helpers/threeHelpers/interfaces/IThreeHelper';
+import { ThreeHelper } from '@/helpers/threeHelpers/core/ThreeHelper';
+import { EnhancedThreeHelper } from '@/helpers/threeHelpers/core/EnhancedThreeHelper';
 
-export default defineComponent({
-  setup() {
-    const canvas = ref<HTMLCanvasElement | null>(null);
+const canvas = ref<HTMLCanvasElement | null>(null);
 
-    onMounted(() => {
-      if (canvas.value) {
-        const baseThreeHelper = new ThreeHelper(canvas.value);
-        const decoratedThreeHelper: IThreeHelper = new EnhancedThreeHelper(baseThreeHelper);
-        decoratedThreeHelper.animate();
-      }
-    });
-
-    return {
-      canvas
-    };
+onMounted(() => {
+  if (canvas.value) {
+    const baseThreeHelper = new ThreeHelper(canvas.value);
+    const decoratedThreeHelper: IThreeHelper = new EnhancedThreeHelper(baseThreeHelper);
+    decoratedThreeHelper.animate();
   }
 });
 </script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
+#canvas {
   display: block;
-  margin: 0 auto 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  outline: none;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
