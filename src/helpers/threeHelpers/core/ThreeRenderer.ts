dec9bb956933203977188ad75ea572e051945088
parhasard <hazar_karadag@hotmail.com>
2023-10-21 23:53:26 +0200

    set pixel ratio

--- src/helpers/threeHelpers/core/ThreeRenderer.ts
+++ src/helpers/threeHelpers/core/ThreeRenderer.ts
@@ -6,6 +6,7 @@ export class ThreeRenderer {
   constructor(canvas?: HTMLCanvasElement) {
     this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas })
     this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.setPixelRatio(window.devicePixelRatio);
     document.body.appendChild(this.renderer.domElement)
   }
 }
